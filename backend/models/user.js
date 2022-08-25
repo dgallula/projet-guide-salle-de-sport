47fbbaeb5dd92e559b53bcd012ace2f76cdf77d4
Your Name <you@example.com>
2022-08-26 02:07:30 +0300

    connected to mongodb

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema ({
+     firstName: { type :String ,require:true},
+     lastName: { type :String ,require:true},
+     dateOfBirth: { type :Date ,require:true},
+    
+})
+
+
+
+  const User= mongoose.model(User,UserSchema)
+
+  module.exports = User
